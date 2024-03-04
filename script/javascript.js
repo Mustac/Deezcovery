2283ae33885f968c96f9a9736a6b55e3380f3caa
Marijan Mustac <mustac.marijan@gmail.com>
2024-03-04 14:37:21 +0100

    fixing menu

--- script/javascript.js
+++ script/javascript.js
@@ -1,11 +1,14 @@
 var navButton = document.getElementById("nav-expand-button");
 var navLinks = document.getElementsByClassName("nav-links")[0];
+var btnImageNavBar = document.getElementById("nav-expand-button");
 
 navButton.addEventListener("click", () =>{
    if(navLinks.classList.contains("nav-links-hiden")){
-    navLinks.classList.remove("nav-links-hiden");
-   }
-   else{
+       navLinks.classList.remove("nav-links-hiden");
+       btnImageNavBar.classList.add("nav-button-active");
+    }
+    else{
+        btnImageNavBar.classList.remove("nav-button-active");
     navLinks.classList.add("nav-links-hiden");
    }
 });
